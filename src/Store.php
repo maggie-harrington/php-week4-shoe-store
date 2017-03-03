fe925d4e9d3cfc672fce17121dfafd82bbcb68fa
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-03 09:11:35 -0800

    add getName method, all tests pass

--- src/Store.php
+++ src/Store.php
@@ -4,10 +4,15 @@
         private $name;
         private $id;
 
-        function __construct($name, $id)
+        function __construct($name, $id = null)
         {
             $this->name = $name;
             $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
         }
     }
 ?>
